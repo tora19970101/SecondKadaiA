7062a60fef52156ac61822f9d104f74d92ef1e73
平澤裕美 <hirasawahiromi@hirasawahiromi-no-macbook-pro.local>
2016-12-07 15:03:37 +0900

    Initial Commit

---
+++ toar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  toar
+//
+//  Created by 平澤裕美 on 2016/12/07.
+//  Copyright © 2016年 toranosuke.sekine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
